538e444aded68c4e484e0550f7a1085f862ba2c3
syun07 <sarahyun7@gmail.com>
2019-04-01 19:33:59 -0400

    fix login/logout

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,8 @@ class AuthController < ApplicationController
 	skip_before_action :authorized, only: [:create]
  
 	def create
-	  @user = User.find_by(name: user_login_params[:name])
+		@user = User.find_by(name: user_login_params[:name])
+		byebug
 	  if @user && @user.authenticate(user_login_params[:password])
 			token = encode_token({ user_id: @user.id })
 			render json: { user: @user, jwt: token }, status: :accepted
